7040a25b4afdd21a9f8619ab9fe94e2b7de68925
ps4417 <ps4417@naver.com>
2020-10-15 13:00:22 +0900

    Update 선택위젯 만들기 과정 설명 추가

--- Android/선택 위젯 만들기/MainActivity.java
+++ Android/선택 위젯 만들기/MainActivity.java
@@ -23,39 +23,45 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
 
     }
-
-    public void setList(final ArrayList<String> datas){  //adapter를 만들어서 listView에 붙인다.
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datas);
+    // 3. adapter 를 만들어서 listView 에 붙인다.
+    public void setList(final ArrayList<String> datas){
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datas);  // simple_list_item_1은  android 에서 제공해주는 부분이므로 그냥 외울 것
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-//                Toast.makeText(MainActivity.this, ""+position, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, ""+position, Toast.LENGTH_SHORT).show();  // 화면에 출력되는지 확인용도로 적어본다.
 
-                //대화상자 뜨게 만들자
+                // 4. 항목을 클릭했을 때 대화상자가 나오도록 만든다.(AlertDialog)
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("HI");
                 builder.setMessage("Do you want to delete this item?"+ datas.get(position));
 
+                // 5. YES 클릭시 항목이 삭제되도록 구현한다.
                 builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        datas.remove(position);  //데이터에서는 삭제됨 (but, 이것만으로는 화면에서 삭제 안된다.!)
-                        adapter.notifyDataSetChanged();  //데이터가 바뀐걸 화면에서 다시 리프레시 해주게된다.
+                        datas.remove(position);  //데이터에서는 삭제됨 (but, 이것만으로는 화면에서 삭제 안된다!)
+                        adapter.notifyDataSetChanged();  //so, 데이터가 바뀐걸 변경해서 화면에 보여주는 nofifySetChanged() 메서드를 사용해야한다.
                     }
                 });
+                // 6. NO 클릭시 아무일도 안일어나도록 아무것도 적지 않는다.
                 builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        //싫으면 아무것도 안하면 됨
+                        // 따로 적을 필요가 없음
                     }
                 });
 
                 builder.show();
+                // 아래처럼 적어도 보여지는건 같다!
+                // AlertDialog dialog = builder.create();
+                // dialog.show();
             }
         });
     }
-
+    // 2. ArrayList<String> 객체를 생성하고 for문을 통해 20개를 담는다.
+    // 2-1. setList() 메서드를 호출한다.
     public void getData(){
         datas = new ArrayList<>();
         for(int i=1;i<=20;i++){
@@ -65,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //화면에서 버튼을 클리하면 데이터를 뿌릴거다.
+    // 1. 버튼을 클릭하면 getData() 메서드를 호출한다.
     public void ckbt(View v){
         getData();
     }
