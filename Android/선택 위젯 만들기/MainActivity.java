30379b537f005aa9da5c97a9df9cbff488e030c1
ps4417 <ps4417@naver.com>
2020-10-15 15:21:02 +0900

    Update Content

--- Android/선택 위젯 만들기/MainActivity.java
+++ Android/선택 위젯 만들기/MainActivity.java
@@ -1,77 +1,97 @@
-package com.example.p426;
+package com.example.p427;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.content.DialogInterface;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
-    ArrayList<String> datas;
+    ArrayList<Person> persons;
+    ConstraintLayout container;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.listView);
-
+        container = findViewById(R.id.container);
     }
-    // 3. adapter 를 만들어서 listView 에 붙인다.
-    public void setList(final ArrayList<String> datas){
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datas);  // simple_list_item_1은  android 에서 제공해주는 부분이므로 그냥 외울 것
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-//                Toast.makeText(MainActivity.this, ""+position, Toast.LENGTH_SHORT).show();  // 화면에 출력되는지 확인용도로 적어본다.
+    // start adapter
+    // 우리만의 adapter 를 만들어야한다.
+    class PersonAdapter extends BaseAdapter{
+        ArrayList<Person> datas;
 
-                // 4. 항목을 클릭했을 때 대화상자가 나오도록 만든다.(AlertDialog)
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("HI");
-                builder.setMessage("Do you want to delete this item?"+ datas.get(position));
-
-                // 5. YES 클릭시 항목이 삭제되도록 구현한다.
-                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        datas.remove(position);  //데이터에서는 삭제됨 (but, 이것만으로는 화면에서 삭제 안된다!)
-                        adapter.notifyDataSetChanged();  //so, 데이터가 바뀐걸 변경해서 화면에 보여주는 nofifySetChanged() 메서드를 사용해야한다.
-                    }
-                });
-                // 6. NO 클릭시 아무일도 안일어나도록 아무것도 적지 않는다.
-                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // 따로 적을 필요가 없음
-                    }
-                });
-
-                builder.show();
-                // 아래처럼 적어도 보여지는건 같다!
-                // AlertDialog dialog = builder.create();
-                // dialog.show();
-            }
-        });
-    }
-    // 2. ArrayList<String> 객체를 생성하고 for문을 통해 20개를 담는다.
-    // 2-1. setList() 메서드를 호출한다.
-    public void getData(){
-        datas = new ArrayList<>();
-        for(int i=1;i<=20;i++){
-            datas.add("Item:"+i);
+        public PersonAdapter(ArrayList<Person> datas){
+            this.datas = datas;
         }
-        setList(datas);
+
+        @Override
+        public int getCount() {  // 항목의 개수를 의미
+            return datas.size();
+        }
+
+        @Override
+        public Object getItem(int position) {  // 항목의 위치를 의미
+            return datas.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {   // n번째 화면을 줄래
+            View view = null;    // 여기서 view 는 한 셀을 의미한다.
+            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.person,container,true); //person.xml의 constraintLayout 의 이름을 container 라고 지정해줬음
+
+            // 3개의 위젯을 가져온다.
+            ImageView im = view.findViewById(R.id.imageView);
+            TextView tx_name = view.findViewById(R.id.tx_name);
+            TextView tx_phone = view.findViewById(R.id.tx_phone);
+
+            Person p = datas.get(position);
+
+            // 셋팅
+            im.setImageResource(p.getId());
+            tx_name.setText(p.getName());
+            tx_phone.setText(p.getPhone());
+            return view;
+        }
+    } //end adapter
+
+
+    public void setList(ArrayList<Person> persons){
+        PersonAdapter personAdapter = new PersonAdapter(persons);
+        listView.setAdapter(personAdapter);
     }
 
-    //화면에서 버튼을 클리하면 데이터를 뿌릴거다.
-    // 1. 버튼을 클릭하면 getData() 메서드를 호출한다.
+    public void getData(){
+        persons = new ArrayList<>();
+        persons.add(new Person(R.drawable.p1,"박민영","010-1234-5678"));
+        persons.add(new Person(R.drawable.p2,"신세경","010-2234-5678"));
+        persons.add(new Person(R.drawable.p3,"설현","010-3234-5678"));
+        persons.add(new Person(R.drawable.p4,"거미","010-4234-5678"));
+        persons.add(new Person(R.drawable.p5,"벤","010-5234-5678"));
+        persons.add(new Person(R.drawable.p6,"유리","010-6234-5678"));
+        persons.add(new Person(R.drawable.p7,"송가인","010-7234-5678"));
+        persons.add(new Person(R.drawable.p8,"블루","010-8234-5678"));
+        persons.add(new Person(R.drawable.p9,"펀치","010-9234-5678"));
+        setList(persons);  //데이터 가져온거 setList 에 뿌려라
+    }
+
     public void ckbt(View v){
         getData();
     }
---
+++ Android/선택 위젯 만들기/Person.java
@@ -0,0 +1,50 @@
+package com.example.p427;
+
+public class Person {
+    int id;
+    String name;
+    String phone;
+
+    public Person(){
+
+    }
+
+    public Person(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
