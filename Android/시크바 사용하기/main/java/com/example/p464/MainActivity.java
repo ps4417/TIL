0534e7f91fd4c832816f9b11a7997a5a413ea3f2
ps4417 <ps4417@naver.com>
2020-10-16 09:59:26 +0900

    시크바 사용하기

---
+++ Android/시크바 사용하기/main/java/com/example/p464/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.p464;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    SeekBar seekBar;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekBar = findViewById(R.id.seekBar);
+        textView = findViewById(R.id.textView);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+               textView.setText(progress+""); //progress 가 int 이므로 뒤에 ""를 더해 String 으로 바꿔준다.
+                setBtight(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+    public void setBtight(int value){
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.screenBrightness = (float)value/100;
+        getWindow().setAttributes(params);
+    }
+}
