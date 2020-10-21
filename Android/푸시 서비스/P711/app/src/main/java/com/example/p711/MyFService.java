3e5b04f5a28641677e2ec446d75bf6e1ecc623cf
ps4417 <ps4417@naver.com>
2020-10-21 17:42:54 +0900

    Update 푸시서비스 설명

--- Android/푸시 서비스/P711/app/src/main/java/com/example/p711/MyFService.java
+++ Android/푸시 서비스/P711/app/src/main/java/com/example/p711/MyFService.java
@@ -14,8 +14,10 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFService extends FirebaseMessagingService {
     public MyFService() {
 
+        // 이 클래스는 구글 클라우드 서버에서 보내오는 메시지를 받을 수 있으며, 메시지가 도착하면 onMessageReceived() 메서드가 자동으로 호출된다.
     }
 
+    // 새로운 메시지를 받았을 때 호출되는 메서드 - 구글 클라우드 서버에서 보내오는 메시지를 받아서 처리할 수 있다.
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         String title = remoteMessage.getNotification().getTitle();
@@ -25,7 +27,7 @@ public class MyFService extends FirebaseMessagingService {
 
 
         // 보내버려 to MainActivity(1)
-        Intent intent = new Intent("notification");  // 이 이름(notification)으로 보낼거야 mainActivity 에서 이 이름으로 받아
+        Intent intent = new Intent("notification");  // 이 이름(notification)으로 mainActivity 에 보낼거야
         intent.putExtra("title",title);
         intent.putExtra("control",control);
         intent.putExtra("data",data);
