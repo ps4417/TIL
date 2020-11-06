06005e587edd054da26208a4668b8bad0f204830
ps4417 <ps4417@naver.com>
2020-11-06 15:19:21 +0900

    Modified MainController - phone.mc

--- Java/웹,모바일,IoT 네트워크연결/IoT에서 Spring서버로 데이터 보내기(추후 센서에서 받은 데이터 전송)/TomcatWebServer(Spring)/tcpip/src/com/tcpip/MainController.java
+++ Java/웹,모바일,IoT 네트워크연결/IoT에서 Spring서버로 데이터 보내기(추후 센서에서 받은 데이터 전송)/TomcatWebServer(Spring)/tcpip/src/com/tcpip/MainController.java
@@ -7,6 +7,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -25,7 +26,7 @@ public class MainController {
 	Ftest ftest;
 	
 	public MainController() {
-		client = new Client("192.168.0.111", 5555, "[WEB]");
+		client = new Client("52.78.89.147", 5555, "[WEB]");
 		try {
 			client.connect();
 		} catch (IOException e) {
@@ -54,58 +55,14 @@ public class MainController {
 	@RequestMapping("/phone.mc")
 	public void phone(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
-		// Ftest 객체가 안불러져와서 Ftest의 service 함수 안에 있는 코드를 복붙함  
-		System.out.println("Phone Send Start ...");
 		
-		URL url = null;
+		Ftest ft = new Ftest();
 		try {
-			url = new URL("https://fcm.googleapis.com/fcm/send");
-		} catch (MalformedURLException e) {
-			System.out.println("Error while creating Firebase URL | MalformedURLException");
+			ft.service(request, response);
+		} catch (ServletException | IOException e) {
 			e.printStackTrace();
 		}
-		HttpURLConnection conn = null;
-		try {
-			conn = (HttpURLConnection) url.openConnection();
-		} catch (IOException e) {
-			System.out.println("Error while createing connection with Firebase URL | IOException");
-			e.printStackTrace();
-		}
-		conn.setUseCaches(false);
-		conn.setDoInput(true);
-		conn.setDoOutput(true);
-		conn.setRequestProperty("Content-Type", "application/json");
-
-		// set my firebase server key
-		conn.setRequestProperty("Authorization", "key="
-				+ "AAAAyJAImqU:APA91bEkTxdYYRXoAMrp1vWQODDzAMoYJle5X2qPUq-J_OZJRxYpwOW17Nh-GO13_gA20wDLI8R7voI6UZ2GOaETREuV0Ja9nxEM0R2ALX2XCkEM1BKyyNrysBlGzvI34G_ILKQNdcuG");
-
-		// create notification message into JSON format
-		JSONObject message = new JSONObject();
-		message.put("to", "/topics/car");
-		message.put("priority", "high");
-		JSONObject notification = new JSONObject();
-		notification.put("title", "IoT프로젝트");
-		notification.put("body", "띵동~ 쿠폰이 도착하였습니다.");
-		message.put("notification", notification);
-		JSONObject data = new JSONObject();
-		data.put("control", "controlWS");
-		data.put("data", 200);
-		message.put("data", data);
-
-
-		try {
-			OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
-			out.write(message.toString());
-			out.flush();
-			conn.getInputStream();
-			System.out.println("OK...............");
-
-		} catch (IOException e) {
-			System.out.println("Error while writing outputstream to firebase sending to ManageApp | IOException");
-			e.printStackTrace();
-		}	
-		
+				
 		
 		PrintWriter out = response.getWriter();
 		out.print("ok");
